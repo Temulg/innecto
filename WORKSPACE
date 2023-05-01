ccd225b5e761805a08d7b931d263766d48b83814
Alex Dubov <oakad@yahoo.com>
2023-05-02 01:18:35 +1000

    Basic groovy integration
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- WORKSPACE
+++ WORKSPACE
@@ -1,1 +1,31 @@
 workspace(name = "innecto")
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+	name = "rules_jvm_external",
+	url = "https://github.com/bazelbuild/rules_jvm_external/archive/4.5.zip",
+	sha256 = "b17d7388feb9bfa7f2fa09031b32707df529f26c91ab9e5d909eb1676badd9a6",
+	strip_prefix = "rules_jvm_external-4.5",
+)
+
+load("@rules_jvm_external//:repositories.bzl", "rules_jvm_external_deps")
+
+rules_jvm_external_deps()
+
+load("@rules_jvm_external//:setup.bzl", "rules_jvm_external_setup")
+
+rules_jvm_external_setup()
+
+load("@rules_jvm_external//:defs.bzl", "maven_install")
+
+maven_install(
+	artifacts = [
+		"org.apache.groovy:groovy:4.0.11",
+	],
+	fetch_sources = True,
+	repositories = [
+		"https://repo1.maven.org/maven2",
+	],
+	version_conflict_policy = "pinned",
+)
--- temulg/innecto/app/App.java
+++ temulg/innecto/app/App.java
@@ -1,19 +1,41 @@
 /*
- * Copyright (c) 2018 Alex Dubov <oakad@yahoo.com>
+ * Copyright (c) 2023 Alex Dubov <oakad@yahoo.com>
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 package temulg.innecto.app;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import groovy.lang.Script;
+import groovy.lang.Binding;
+import groovy.lang.GroovyCodeSource;
+import groovy.lang.GroovyClassLoader;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.runtime.InvokerHelper;
+
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Boooo! Booooo!");
-		var z = (Object)"aaa";
-		switch (z) {
-		case String s -> System.out.println("iippii " + s);
-		case Integer x -> {}
-		default -> System.out.println("zzzz");
+	public static void main(String[] args) throws Exception {
+		System.out.println("File in " + args[0]);
+
+		var loader = new GroovyClassLoader(
+			App.class.getClassLoader(),
+			CompilerConfiguration.DEFAULT
+		);
+
+		Class scriptClass;
+
+		try (var src = new FileReader(args[0])) {
+			var cs = new GroovyCodeSource(
+				src, "testing_Package", "/testing"
+			);
+			scriptClass = loader.parseClass(cs);
 		}
+
+		System.out.println("Class out: " + scriptClass.getName());
+
+		InvokerHelper.newScript(scriptClass, new Binding()).run();
 	}
 }
